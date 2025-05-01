7332471c6cfd38835c54be158c217445f429ba72
Delia Paternina <delia.paternina@gmail.com>
2025-05-01 10:12:42 -0400

    doubles the size of the steps

--- chapter-1/binary-search/README.md
+++ chapter-1/binary-search/README.md
@@ -6,3 +6,6 @@
 `` Log2(128) = 7 ``
 
 ## 1.2 Suppose you double the size of the list. What's the maximum number of steps now?
+
+`` Maximum number of steps = Log2(n) ``
+`` Log2(256) = 8 ``
--- chapter-1/binary-search/binary_search.js
+++ chapter-1/binary-search/binary_search.js
@@ -1,5 +1,6 @@
 /**
- * Binary search works by diving the list in half each time. The maximum number of guesses is the number of times you can divide the list in half before you get to just 1 item.
+ * Binary search works by diving the list in half each time. The maximum number of guesses is the number of times
+ * you can divide the list in half before you get to just 1 item.
  * @param {number} n
  * @returns {number}
  */
@@ -8,4 +9,10 @@ function maxBinarySearchSteps(n) {
   // Math.ceil() is a buit-in JavaScript function that rounds a number up to the nearest integer. It always returns the smallest integer greater than or equal to the given number.
   return Math.ceil(Math.log2(n));
 }
-console.log(maxBinarySearchSteps(128));
+console.log("Steps for 128 items:", maxBinarySearchSteps(128));
+
+function doubleListSizeAndGetSteps(n) {
+  const doubledSize = n * 2;
+  return maxBinarySearchSteps(doubledSize);
+}
+console.log("Steps for 256 items:", doubleListSizeAndGetSteps(128));
