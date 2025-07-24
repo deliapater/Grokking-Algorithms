ec4a37ca1d4e2fd50612d501e9ff0b1e3a493b18
Delia Paternina <delia.paternina@gmail.com>
2025-07-24 13:42:17 +0100

    adds max number excercise

--- chapter-4/README.md
+++ chapter-4/README.md
@@ -3,3 +3,5 @@
 ### 4.1 Write out the code for the earlier `sum` function.
 
 ### 4.2 Write a recursive function to count the number of items in a list. 
+
+### 4.3 Find the maximum number in a list..
--- chapter-4/divideAndConquer.js
+++ chapter-4/divideAndConquer.js
@@ -5,11 +5,11 @@
  * @returns {number} Total sum
  */
 function sum(arr) {
-    if (arr.length === 0) return 0;
-    return arr[0] + sum(arr.slice(1));
+  if (arr.length === 0) return 0;
+  return arr[0] + sum(arr.slice(1));
 }
 console.log("\n--- Array Sum Demo ---");
-console.log(sum([1,2,3,4]))
+console.log(sum([1, 2, 3, 4]));
 
 /** Excercise 4.2
  * Recursively counts the number of elements in an array.
@@ -17,9 +17,24 @@ console.log(sum([1,2,3,4]))
  * @returns {number} The number of items
  */
 function countItems(arr) {
-    if (arr.length === 0) return 0;
-    return 1 + countItems(arr.slice(1));
+  if (arr.length === 0) return 0;
+  return 1 + countItems(arr.slice(1));
 }
 
 console.log("\n--- Number of Items Demo ---");
 console.log(countItems([10, 20, 30, 40]));
+
+/**
+ * Recursively finds the maximum number in a array
+ * @param {number []} arr - Array of numbers
+ * @returns {number} Maximum value
+ */
+function findMax(arr) {
+  if (arr.length === 1) {
+    return [0];
+  }
+  const subMax = findMax(arr.slice(1));
+  return arr[0] > subMax ? arr[0] : subMax;
+}
+console.log("\n--- Max Number Demo ---");
+console.log(findMax([3, 9, 2, 5, 11, 4]));
